9a931f56d4c8c601361ec71a5a4ef18da8f54cad
Tom <petridistom@gmail.com>
2023-05-26 17:28:58 +1000

    finished

--- public/sketch.js
+++ public/sketch.js
@@ -10,19 +10,19 @@ function setup() {
 function draw() {
   background(0);
   
-  let sig = sin (frameCount * TWO_PI / 360) 
+  let sig = sin (frameCount * TWO_PI / 360) // [ 0, 1 ]
+  console.log(sig)
   // (360 / 24 fps) = 15s seconds per breath cycle
   
   sig += 1   // [ 000 , 002]
   sig *= 500 // [ 000 , 1000]
   sig += 10  // [ 010 , 1010]
-  
-  // console.log(sig) // check the size of the ball
-  
+    
   fill('deeppink')
+
   drawingContext.shadowBlur = 128;
   drawingContext.shadowColor = 'deeppink'
-  
+
   circle ( width / 2, windowHeight, sig)
 }
 
