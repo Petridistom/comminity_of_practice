fe1ff66275293ce7c79568d0e5a2c88e2ea112b1
Tom <petridistom@gmail.com>
2023-05-26 16:57:04 +1000

    draw frame

--- public/sketch.js
+++ public/sketch.js
@@ -1,88 +1,46 @@
-document.body.style.margin   = 0
-document.body.style.overflow = `hidden`
+//animating can be done using requestAnimationFrame:
+// HOW TO WRITE THE P5 DRAW FUNCTION IN PLANE JAVASCRIPT
 
-// initate variables for drawing 
-// the line
-let spacing   = 1;
-let speed     = 0;
-let amplitude = 125;
-let period;
-let w;
+// remove padding and scroll bar
+document.body.style.margin = 0
+document.body.style.overflow = 'hidden'
 
-// initate variables for 
-// colours
-let col  = 170;
-let rate = 0.2;
+const cnv = document.createElement('canvas');
+cnv.width  = window.innerWidth
+cnv.height = window.innerHeight
+document.body.appendChild(cnv)
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  
-  // set the length of the line to the 
-  // width of the window
-  w = width;
-  
-  // create a new array that is the length
-  // of (window / spacing)
-  yValues = new Array(floor(w / spacing)); 
+const ctx = cnv.getContext('2d')
+                
+// pass in the name of the function
+// requestAnimationFrame will call it
+requestAnimationFrame (draw_frame)
+
+// define the function you want
+// requestAnimationFrame to call it
+function draw_frame () {
+
+    // draw the background
+    ctx.fillStyle = 'turquoise'
+    ctx.fillRect (0, 0, cnv.width, cnv.height)
+
+    // draw the pink square at its current x-coordinate
+    ctx.fillStyle = 'deeppink'
+    ctx.fillRect (x_pos, cnv.height / 2, 100, 100)
+
+    // increment the x_pos
+    x_pos += 2
+
+    // respawn it on the left once it leaves the frame
+    if (x_pos > cnv.width) {
+        x_pos = -100
+    }
+
+    // call the next frame
+    requestAnimationFrame(draw_frame)
 }
 
-function draw() {
+// dont forget to call requestAnimationFrame inside the draw_frame function
+// so that it recalls itself and always draws the next frame
 
-  // defined below
-  col_change();
-  
-  // set the background colour
-  fill('white')
-  square(0, 0, width)
-  background(205, 114, col, 220);
-  
-  // defined below
-  calculate_line ();
-  display_line();
-}
-
-// define a fuction that calculates the positioning
-// of the line in the window
-function calculate_line () {
-  
-  // increases the speed of the line
-  speed += 0.008;
-  
-  // initiate x variable and set it to speed
-  let x = speed
-  
-  // for every x, calculate a y value on a sine wave
-  for (let i = 0; i < yValues.length; i++) {
-    yValues[i] = sin(x) * amplitude
-  }
-  
-}
-
-function display_line () {
-  // remove stroke
-  noStroke()
-  
-  // set colour to aliceblue
-  fill('alicelue')
-  
-  // for every x value of the line, draw a rect
-    for (let x = 0; x < yValues.length; x++) {
-    rect(x * spacing, height / 2 + yValues[x], 16, 6);
-  }
-}
-
-function col_change () {
-  
-  // change the colour value by the rate
-    col += rate;
-  
-  // if the value leaves the range, reverse the rate
-  if (col >= 255 || col <= 170) {
-    rate *= -1
-  }
-}
-
-// adds resizability
-function windowResized () {
-  resizeCanvas (window.innerWidth, window.innerHeight)
-}
+// this is a type of recursive function, a function that calls itself
